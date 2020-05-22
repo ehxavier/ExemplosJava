d390da329da3a0bcf136499a74da6290e2d831f5
Emanuel Xavier <ehxavier@gmail.com>
2020-05-22 11:17:02 -0300

    Atualização dos exemplos de códigos Java.

--- src/exemplosjava/ExemplosJava.java
+++ src/exemplosjava/ExemplosJava.java
@@ -1,21 +1,55 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+
 package exemplosjava;
 
-/**
- *
- * @author senac
- */
 public class ExemplosJava {
 
-    /**
-     * @param args the command line arguments
-     */
     public static void main(String[] args) {
-        // TODO code application logic here
+        /*
+        Java é uma linguagem de programação orientada a objetos desenvolvida na década de 90 
+        por uma equipe de programadores chefiada por James Gosling, 
+        na empresa Sun Microsystems.
+        */
+        
+        // Declarando variáveis...
+        String nome, sobrenome;
+        int idade;
+        char sexo;
+        double altura;
+        boolean cadastrado = true;
+        
+        // Atribuindo valores às variáveis...
+        nome = "Joaquim";
+        sobrenome = "Santos";
+        idade = 32;
+        sexo = 'M';
+        altura = 1.79; 
+        
+        // Comandos de saida... Hello World!
+        System.out.println("Hello World!");
+        System.out.println(nome);
+        System.out.println(nome + " " + sobrenome);
+        
+        int n1 = 3;
+        int n2 = 5;
+        System.out.println(n2%n1);
+        
+        System.out.println(n1 == n2);  //false
+        System.out.println(n1 != n2);  //true
+        
+        n1++; // 3 + 1 = 4
+        n2--; // 5 - 1 = 4
+        System.out.println(n2/n1);
+        
+        System.out.println(n1 == n2); //true
+        // imprima(n1=n2)  VERDADEIRO
+        System.out.println(n1 != n2); //false
+        // imprima(n1<>n2) FALSO
+        
+        System.out.println("Joaquim" + " " + "Silva");
+        System.out.println("n1 + n2 = " + (n1+n2) );
+        
+        
+        
     }
     
 }
